646f86cb4ff2a76b0865481299cd6ea19e64f220
ChrisKnowsDev <chrisharley81@gmail.com>
2025-10-20 13:22:08 -0400

    create image carousel in work section

---
+++ src/components/work/gallery-slider.tsx
@@ -0,0 +1,43 @@
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import type { ImageMetadata } from 'astro'
+const modules = import.meta.glob<{ default: ImageMetadata }>(
+  '../../images/work/*',
+  {
+    eager: true,
+  },
+)
+
+const images = Object.values(modules).map((module) => module.default.src)
+
+export function GallerySlider() {
+  return (
+    <Carousel className="mx-auto w-full max-w-6xl">
+      <CarouselContent className="-ml-4">
+        {images.map((src, index) => (
+          <CarouselItem key={index} className="pl-4 md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="aspect-square items-center justify-center">
+                  <img
+                    src={src}
+                    alt=""
+                    className="h-full w-full object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="left-4" />
+      <CarouselNext className="right-4" />
+    </Carousel>
+  )
+}
